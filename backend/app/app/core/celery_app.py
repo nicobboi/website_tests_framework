4dfb7ba030c0a398f44b752bf29ebba85f199922
Mauro Amico <mauro.amico@gmail.com>
2023-08-02 18:04:53 +0200

    beat

--- backend/app/app/core/celery_app.py
+++ backend/app/app/core/celery_app.py
@@ -2,12 +2,11 @@ from celery import Celery
 
 # celery app configuration
 celery_app = Celery('worker')
-celery_app.conf.broker_url = 'redis://redis:6379/0'
-celery_app.conf.result_backend = 'redis://redis:6379/0'
+celery_app.conf.broker_url = 'redis://redis:6379'
+celery_app.conf.result_backend = 'redis://redis:6379'
 
 celery_app.conf.update(
     CELERY_REDIS_SCHEDULER_URL = 'redis://redis:6379',
-    beat_schedule = {}
 )
 
 celery_app.conf.timezone = "Europe/Rome"
